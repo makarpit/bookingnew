e1f04f74e74db3e073c7b2ed85070065397f580d
ajiteshverma <ajitesh.verma@adglobal360.com>
2019-11-12 22:30:02 +0530

    push code to git master repository

--- production/software/blog/admin.py
+++ production/software/blog/admin.py
@@ -1,11 +1,47 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import Blogpost, Page, NewsAndEvent, Testimonial, Member
 
-admin.site.register(Blogpost)
-admin.site.register(Page)
-admin.site.register(NewsAndEvent)
-admin.site.register(Testimonial)
-admin.site.register(Member)
+class BlogpostAdmin(admin.ModelAdmin):
+	  list_display = ['title','head0','head1']
+	  search_fields = ['title','head0','head1']
+	  list_filter = ['title','head0','head1']
+
+
+	  
+admin.site.register(Blogpost, BlogpostAdmin)
+
+class PageAdmin(admin.ModelAdmin):
+	  list_display = ['name','meta_title','image']
+	  search_fields = ['name','meta_title','image']
+	  list_filter = ['name','meta_title']
+
+
+admin.site.register(Page, PageAdmin)
+
+class NewsAndEventAdmin(admin.ModelAdmin):
+	  list_display = ['name','meta_title','image']
+	  search_fields = ['name','meta_title','image']
+	  list_filter = ['name','meta_title']
+
+
+
+admin.site.register(NewsAndEvent, NewsAndEventAdmin)
+
+class TestimonialAdmin(admin.ModelAdmin):
+	  list_display = ['name','meta_title','image']
+	  search_fields = ['name','meta_title','image']
+	  list_filter = ['name','meta_title']
+
+
+
+admin.site.register(Testimonial, TestimonialAdmin)
+
+class MemberAdmin(admin.ModelAdmin):
+	  list_display = ['name','meta_title','image']
+	  search_fields = ['name','meta_title','image']
+	  list_filter = ['name','meta_title']
+
+
+
+admin.site.register(Member, MemberAdmin)
 
--- production/software/shop/admin.py
+++ production/software/shop/admin.py
@@ -3,7 +3,38 @@ from django.contrib import admin
 # Register your models here.
 from .models import Product, Contact, Orders, OrderUpdate
 
-admin.site.register(Product)
-admin.site.register(Contact)
-admin.site.register(Orders)
-admin.site.register(OrderUpdate)
+class ProductAdmin(admin.ModelAdmin):
+	  list_display = ['product_name','category','price']
+	  search_fields = ['product_name','category','price']
+	  list_filter = ['product_name','category','price']
+
+
+
+admin.site.register(Product, ProductAdmin)
+
+class ContactAdmin(admin.ModelAdmin):
+	  list_display = ['name','email','phone']
+	  search_fields = ['name','email','phone']
+	  list_filter = ['name','email','phone']
+
+
+
+admin.site.register(Contact, ContactAdmin)
+
+class OrdersAdmin(admin.ModelAdmin):
+	  list_display = ['name','amount','city']
+	  search_fields = ['name','amount','city']
+	  list_filter = ['name','amount','city']
+
+
+
+admin.site.register(Orders, OrdersAdmin)
+
+class OrderUpdateAdmin(admin.ModelAdmin):
+	  list_display = ['order_id','update_desc','timestamp']
+	  search_fields = ['order_id','update_desc','timestamp']
+	  list_filter = ['order_id','update_desc','timestamp']
+
+
+
+admin.site.register(OrderUpdate, OrderUpdateAdmin)
